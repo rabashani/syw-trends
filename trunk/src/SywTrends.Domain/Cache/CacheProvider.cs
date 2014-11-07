0b5cdfdfcc4849acd0c6516ed44fddcdb209719b
shani <shani@sears.co.il>
2014-11-07 11:36:47 +0200

    improving the cache mechanism and using it to store the interactions

--- trunk/src/SywTrends.Domain/Cache/CacheProvider.cs
+++ trunk/src/SywTrends.Domain/Cache/CacheProvider.cs
@@ -1,15 +1,70 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Runtime.Caching;
 
 namespace SywTrends.Domain.Cache
 {
 	public interface ICacheProvider
 	{
+	    void Clean(string key);
+	    bool IsExists(string key);
 		T Get<T>(string key, Func<T> loader);
+	    T[] AddOrCreate<T>(string key, Func<T> loader);
 	}
 
 	public class CacheProvider : ICacheProvider
 	{
+        public bool IsExists(string key)
+        {
+            var cacheManger = MemoryCache.Default;
+            if (!cacheManger.Contains(key) || cacheManger[key] == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Clean(string key)
+        {
+            var cacheManger = MemoryCache.Default;
+            cacheManger.Remove(key);
+        }
+
+        public T[] AddOrCreate<T>(string key, Func<T> loader)
+        {
+            T singleResult;
+            List<T> allResults;
+            var cacheManger = MemoryCache.Default;
+            var objresult = cacheManger[key];
+
+   			if (!cacheManger.Contains(key) || cacheManger[key] == null)
+			{
+				var policy = new CacheItemPolicy();
+				var funcResult = loader();
+
+				if (funcResult == null)
+					singleResult = default(T);
+				else
+					singleResult = (T)funcResult;
+
+   			    allResults = new List<T>{singleResult};
+
+				cacheManger.Add(key, allResults, policy);
+			}
+			else
+   			{
+   			    allResults = (List<T>)cacheManger[key];
+   			    var funcResult = loader();
+                allResults.Add((T)funcResult);
+   			    cacheManger[key] = allResults;
+   			}
+
+
+            return allResults.ToArray();
+        }
+
 		public T Get<T>(string key, Func<T> loader)
 		{
 			T result;
--- trunk/src/SywTrends.Web.UI/Controllers/InteractionsController.cs
+++ trunk/src/SywTrends.Web.UI/Controllers/InteractionsController.cs
@@ -6,6 +6,7 @@ using System.Web.Helpers;
 using System.Web.Mvc;
 using Platform.Client;
 using Platform.Client.Configuration;
+using SywTrends.Domain.Cache;
 using SywTrends.Domain.Interests;
 using SywTrends.Domain.Users;
 
@@ -14,15 +15,17 @@ namespace SywTrends.Web.UI.Controllers
 	public class InteractionsController : Controller
 	{
 		private readonly IApplicationSettings _applicationSettings;
+	    private readonly ICacheProvider _cache;
 
-	    public InteractionsController(IApplicationSettings applicationSettings)
-		{
-			_applicationSettings = applicationSettings;
-		}
+	    public InteractionsController(IApplicationSettings applicationSettings, ICacheProvider cache)
+	    {
+	        _applicationSettings = applicationSettings;
+	        _cache = cache;
+	    }
 
-        //Collect user Interactions /interactions/collect
+	    //Collect user Interactions /interactions/collect
         [HttpGet]
-        public ActionResult Collect()
+        public ActionResult Collect(string content)
 		{
 			// Making sure the application is configured correctly and the application is called from a canvas
 			// Delete this code once this is done
@@ -36,9 +39,26 @@ namespace SywTrends.Web.UI.Controllers
 				return Json(new {error="configuarion-errors-exception"}, JsonRequestBehavior.AllowGet);
 			}
 
-            return Json(new { foo = "bar" }, JsonRequestBehavior.AllowGet);
+            var value = StoreLocal(content);
+
+            return Json(new { value }, JsonRequestBehavior.AllowGet);
 		}
 
+        private string StoreLocal(string content)
+        {
+            if (string.IsNullOrEmpty(content))
+                return "empty";
+
+            var value = _cache.Get("interactions", () =>  content );
+            return value;
+        }
+
+        [HttpGet]
+        public ActionResult Clean()
+        {
+            _cache.Clean("interactions");
+            return Json(new {status = "done"}, JsonRequestBehavior.AllowGet);
+        }
 
 	}
 }
