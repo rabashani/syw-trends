51140e71538e74a4dad23b718a261b07fb4944fd
shani <shani@sears.co.il>
2014-11-07 10:54:01 +0200

    change configuration + added listener for interaction\collect

---
+++ trunk/src/SywTrends.Web.UI/Controllers/InteractionsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using Platform.Client;
+using Platform.Client.Configuration;
+using SywTrends.Domain.Interests;
+using SywTrends.Domain.Users;
+
+namespace SywTrends.Web.UI.Controllers
+{
+	public class InteractionsController : Controller
+	{
+		private readonly IApplicationSettings _applicationSettings;
+
+	    public InteractionsController(IApplicationSettings applicationSettings)
+		{
+			_applicationSettings = applicationSettings;
+		}
+
+        //Collect user Interactions /interactions/collect
+        [HttpGet]
+        public ActionResult Collect()
+		{
+			// Making sure the application is configured correctly and the application is called from a canvas
+			// Delete this code once this is done
+			try
+			{
+				var appId = _applicationSettings.AppId;
+				var appSecret = _applicationSettings.AppSecret;
+			}
+			catch (ConfigurationErrorsException)
+			{
+				return Json(new {error="configuarion-errors-exception"}, JsonRequestBehavior.AllowGet);
+			}
+
+            return Json(new { foo = "bar" }, JsonRequestBehavior.AllowGet);
+		}
+
+
+	}
+}
